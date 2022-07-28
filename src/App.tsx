a84e25fe88fb8397c57bcd03b8b4e952c3fc1554
AlesanderZabrodski09 <zabrodski666@gmail.com>
2022-07-28 06:01:19 +0200

    create: introductions and         nesting in react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {NavLink, Outlet, Route, Routes} from 'react-router-dom';
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavLink to={'/'}>main</NavLink>---
+      <NavLink to={'/login'}>login</NavLink>---
+      <NavLink to={'/profile'}>profile</NavLink>---
+      <NavLink to={'/profile/1'}>settings</NavLink>
+
+      <Routes>
+        <Route path={'/*'} element={<div>404</div>}/>
+        <Route path={'/'} element={<div>main</div>}/>
+        <Route path={'/login'} element={<div>login</div>}/>
+        <Route path={'/profile/:id'} element={<div>login</div>}/>
+
+
+        {/*<Route path={'/profile'} element={(*/}
+        {/*  <div>*/}
+        {/*    profile*/}
+        {/*    <Outlet/>*/}
+        {/*  </div>*/}
+        {/*)}>*/}
+        {/*  <Route path={'*'} element={<div>profile page not found</div>}/>*/}
+        {/*  <Route index element={<div>check id</div>}/>*/}
+        {/*  <Route path={':id'} element={<div>id</div>}/>*/}
+        {/*  <Route path={'settings'} element={<div>settings</div>}/>*/}
+        {/*</Route>*/}
+
+
+
+        {/*<Route path={'/profile/*'} element={(*/}
+        {/*  <div>*/}
+        {/*    profile*/}
+        {/*    <Routes>*/}
+        {/*      <Route path={'/settings'} element={<div>settings</div>}/>*/}
+        {/*    </Routes>*/}
+        {/*  </div>*/}
+        {/*)}/>*/}
+      </Routes>
     </div>
   );
 }
