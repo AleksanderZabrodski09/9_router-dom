b3891d5ee45dca98e344e54420aa3d5f23eae4b7
AlesanderZabrodski09 <zabrodski666@gmail.com>
2022-07-28 06:41:45 +0200

    create: add work Links

--- src/App.tsx
+++ src/App.tsx
@@ -3,28 +3,39 @@ import './App.css';
 import {NavLink, Outlet, Route, Routes, useParams} from 'react-router-dom';
 
 
-const Profile=()=>{
-  const params = useParams<'*'>()
-  const some=params['*']
-  console.log(some)
-return <div>profile</div>
-}
-
-
-
 function App() {
   return (
     <div className="App">
       <NavLink to={'/'}>main</NavLink>---
       <NavLink to={'/login'}>login</NavLink>---
-      <NavLink to={'/profile'}>profile</NavLink>---
-      <NavLink to={'/profile/1'}>profile/1</NavLink>
+      <NavLink
+        to={'/profile'}
+        style={(params) => {
+          return {color: params.isActive ? 'lime' : 'black'}
+        }}
+      >
+        profile
+      </NavLink>---
+      <NavLink
+        to={'/profile/settings'}
+        className={({isActive}) => isActive ? 'act' : 'def'}
+      >
+        settings
+      </NavLink>---
 
+      <a
+        href={'https://www.udemy.com/course/the-complete-javascript-course/'}
+        target={'_blank'}
+        rel={'noreferrer nofollow noopener '}
+      >
+        xxx
+      </a>
       <Routes>
         <Route path={'/*'} element={<div>404</div>}/>
         <Route path={'/'} element={<div>main</div>}/>
         <Route path={'/login'} element={<div>login</div>}/>
-        <Route path={'/profile/*'} element={<Profile/>}/>
+        <Route path={'/profile'} element={<div>profile</div>}/>
+        <Route path={'/profile/settings'} element={<div>settings</div>}/>
 
       </Routes>
     </div>
